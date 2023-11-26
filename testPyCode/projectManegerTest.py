06d24301f7f2cc9c4585bbad2c89d0c0544bdf98
ejm-knt <syokugyoukunren.ejima@gmail.com>
2023-11-27 02:12:25 +0900

    projectManegerTest.py 新規作成 ・listの変数名を変えて動的にリストを新規作成したかったので関数化 ・BingAIくんに聞いて、動的にリストを増やす方法ができる関数を用意してもらった 参考：https://sl.bing.net/h4ecK9p8jM4

---
+++ testPyCode/projectManegerTest.py
@@ -0,0 +1,41 @@
+class Task:
+    def __init__(self, content, date):
+        self.content = content  # タスクの内容
+        self.date = date  # タスクの日付
+
+    def __repr__(self):
+        return f'Task(content={self.content}, date={self.date})'
+
+class ProjectManager:
+    def __init__(self):
+        self.projects = {}  # 各プロジェクトを格納するディクショナリ
+
+    # プロジェクトを追加する
+    def add_project(self, name):
+        self.projects[name] = []
+
+    # タスクを追加する
+    def add_task(self, project_name, task):
+        if project_name in self.projects:
+            self.projects[project_name].append(task)
+        else:
+            print(f"プロジェクト {project_name} は存在しません。")
+
+    # プロジェクトとそのタスクを表示する
+    def display_projects(self):
+        for project, tasks in self.projects.items():
+            print(f"{project}: {tasks}")
+
+# プロジェクトマネージャーを作成
+pm = ProjectManager()
+
+# プロジェクトとタスクを追加
+pm.add_project("pj1")
+pm.add_task("pj1", Task("task1", "2023-11-26"))
+
+pm.add_project("pj2")
+pm.add_task("pj2", Task("task1", "2023-11-27"))
+pm.add_task("pj2", Task("task2", "2023-11-28"))
+
+# 結果を表示
+pm.display_projects()
