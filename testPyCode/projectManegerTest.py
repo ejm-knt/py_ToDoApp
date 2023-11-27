374529dce856951676604125eaefb63a66416a81
ejm-knt <syokugyoukunren.ejima@gmail.com>
2023-11-27 11:51:24 +0900

    pjmTest.py 機能追加 ・プロジェクトを削除する関数を追加

--- testPyCode/projectManegerTest.py
+++ testPyCode/projectManegerTest.py
@@ -12,7 +12,17 @@ class ProjectManager:
 
     # プロジェクトを追加する
     def add_project(self, name):
-        self.projects[name] = []
+        if not (name in self.projects) :
+            self.projects[name] = []
+        else:
+            print(f"プロジェクト {name} は既に存在しています。")
+
+    # プロジェクトを削除する
+    def remove_project(self, project_name):
+        if project_name in self.projects:
+            del self.projects[project_name]
+        else:
+            print(f"プロジェクト {project_name} は存在しません。")
 
     # タスクを追加する
     def add_task(self, project_name, task):
@@ -20,6 +30,13 @@ class ProjectManager:
             self.projects[project_name].append(task)
         else:
             print(f"プロジェクト {project_name} は存在しません。")
+
+    # # タスクを削除する
+    # def remove_task(self, project_name, task, index):
+    #     if project_name in self.projects and :
+            
+    #     else:
+    #         print(f"プロジェクト {project_name} は存在しません。")
 
     # プロジェクトとそのタスクを表示する
     def display_projects(self):
@@ -30,6 +47,9 @@ class ProjectManager:
 pm = ProjectManager()
 
 # プロジェクトとタスクを追加
+
+# while True:
+#     input("1:pjAdd 2:pjDel 3:taskAdd 4:taskDel")
 pm.add_project("pj1")
 pm.add_task("pj1", Task("task1", "2023-11-26"))
 
@@ -39,3 +59,5 @@ pm.add_task("pj2", Task("task2", "2023-11-28"))
 
 # 結果を表示
 pm.display_projects()
+
+print(pm.projects["pj1"])
