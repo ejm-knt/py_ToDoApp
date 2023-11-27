4194518908f3d2d3187ddf135edb9c609eb15412
ejm-knt <syokugyoukunren.ejima@gmail.com>
2023-11-27 15:02:46 +0900

    taskManegerClass.py 新規作成 ・task内に記載していた各機能をクラス化し別ファイルに記述。 ・本番用

---
+++ taskManegerClass.py
@@ -0,0 +1,39 @@
+class ProjectManager:
+    def __init__(self):
+        self.projects = {}  # 各プロジェクトを格納するディクショナリ
+
+    # プロジェクトを追加する
+    def add_project(self, name):
+        if not (name in self.projects) :
+            self.projects[name] = []
+        else:
+            print(f"プロジェクト {name} は既に存在しています。")
+
+    # プロジェクトを削除する
+    def remove_project(self, project_name):
+        if project_name in self.projects:
+            del self.projects[project_name]
+        else:
+            print(f"プロジェクト {project_name} は存在しません。")
+
+    # タスクを追加する
+    def add_task(self, project_name, task):
+        if project_name in self.projects:
+            self.projects[project_name].append(task)
+        else:
+            print(f"プロジェクト {project_name} は存在しません。")
+
+    # タスクを削除する
+    def remove_task(self, project_name, index):
+        if project_name in self.projects:
+            try:
+                self.projects[project_name].pop(index)
+            except IndexError:
+                print(f"プロジェクト {project_name} にはインデックス {index} のタスクは存在しません。")
+        else:
+            print(f"プロジェクト {project_name} は存在しません。")
+
+    # プロジェクトとそのタスクを表示する
+    def display_projects(self):
+        for project, tasks in self.projects.items():
+            print(f"{project}: {tasks}")
