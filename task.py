670b639590ee24dfa2a54043e6ce76d202e5e5e0
ejm-knt <syokugyoukunren.ejima@gmail.com>
2023-11-27 15:23:46 +0900

    task.py 修正 ・各種外部クラスの適応 及び sesionCount無効化などのことをした 　→　sessionCountはわんちゃん使わないかもなので、一旦削除。

--- task.py
+++ task.py
@@ -1,49 +1,41 @@
 # Flaskとその他の必要なライブラリをインポート
 from flask import Flask, render_template, request, session
-from datetime import datetime
 
-# タスククラスの定義
-class Task:
-    def __init__(self, content, date, isView=True):
-        self.content = content  # タスクの内容
-        self.date = date  # タスクの日付
-        self.isView = isView  # タスクの表示状態
+from taskClass import Task
+from taskManegerClass import ProjectManager
 
 # Flaskアプリケーションのインスタンスを作成
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'  # セッションを安全に使用するための秘密鍵
 
-# タスクリストの初期化
-pjDict = {}
-tasks = []
+# プロジェクトマネージャーclassの取得/定義
+pjm = ProjectManager()
 
 # ルートURLへのアクセスを処理
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # セッションにカウントが存在しない場合は初期化
-    if 'count' not in session:
-        session['count'] = 0
-
     # POSTリクエストの場合
     if request.method == 'POST':
 
-        # タスクが送信された場合
-        if request.form.get('task'):
-            content = request.form.get('task')  # タスクの内容を取得
-            date = datetime.now().strftime('%Y-%m-%d %H:%M')  # 現在の日時を取得
-            tasks.append(Task(content, date))  # タスクをリストに追加
-
-        # 'Add'ボタンが押された場合
-        elif request.form.get('add') == 'pjAdd':
-            session['count'] += 1  # カウントを増やす
-            
+        # pjAddが送信された場合
+        if request.form.get('pjAdd'):
+            request.form.get('pjAdd') == 'createPJ'
+            pjm.add_project(request.form.get('newPjName'))
 
         # 'Remove'ボタンが押され、かつカウントが0より大きい場合
-        elif request.form.get('remove') == 'pjRemove' and session['count'] > 0:
-            session['count'] -= 1  # カウントを減らす
+        if request.form.get('remove') == 'pjRemove':
+            removed_value = d.pop('k1')
+            print(d)
+            # {'k2': 2, 'k3': 3}
+
+        # tasksAddが送信された場合
+        if request.form.get('newPjName'):
+            content = request.form.get('newPjName')  # タスクの内容を取得
+            tasks.append(Task(content, date))  # タスクをリストに追加
+
 
     # index.htmlをレンダリングしてレスポンスを返す
-    return render_template('index.html', tasks=tasks, pjDict=pjDict, count=session['count'])
+    return render_template('index.html', pjmDict = pjm.projects, count=session['count'])
 
 # スクリプトが直接実行された場合にサーバーを起動
 if __name__ == '__main__':
